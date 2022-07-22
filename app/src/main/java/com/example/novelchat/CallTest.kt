ef6f1ed6e2a68941ff19ace1af579b413eeeb4ef
hermioneee2 <hermioneee2@gmail.com>
2022-07-23 02:50:55 +0900

    agora basic voice call done (with web, temp token)

--- app/src/main/java/com/example/novelchat/CallTest.kt
+++ app/src/main/java/com/example/novelchat/CallTest.kt
@@ -2,24 +2,61 @@ package com.example.novelchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import io.agora.rtc.RtcEngine
+import io.agora.rtc.IRtcEngineEventHandler
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
 import android.util.Log
-import android.widget.*
-import androidx.core.widget.addTextChangedListener
-import io.socket.emitter.Emitter
+import java.lang.Exception
 
 class CallTest : AppCompatActivity() {
+    private val PERMISSION_REQ_ID_RECORD_AUDIO = 22
+//    private val PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1
+
+    // Fill the App ID of your project generated on Agora Console.
+    private val APP_ID = "f87f901b9b6c4a6aa4b2bdf5edea1331"
+    // Fill the channel name.
+    private val CHANNEL = "0723test"
+    // Fill the temp token generated on Agora Console.
+    private val TOKEN = "006f87f901b9b6c4a6aa4b2bdf5edea1331IABIDxQWYqCRjQgAZKiLYt8dsPkd6sGEF8+0k4oJhbSLTSjSI9sAAAAAEAAtDEjTvxrcYgEAAQC+Gtxi"
+    private var mRtcEngine: RtcEngine ?= null
+    private val mRtcEventHandler = object : IRtcEngineEventHandler() {
+    }
+
+    private fun checkSelfPermission(permission: String, requestCode: Int): Boolean {
+        if (ContextCompat.checkSelfPermission(this, permission) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(permission),
+                requestCode)
+            return false
+        }
+        return true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_test)
 
-//        val room_num = findViewById<TextView>(R.id.room_num)
-//        val enter_text = findViewById<EditText>(R.id.type_text)
-//        val send = findViewById<Button>(R.id.send_button)
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO)) {
+            initializeAndJoinChannel();
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mRtcEngine?.leaveChannel()
+        RtcEngine.destroy()
+    }
 
-
+    private fun initializeAndJoinChannel() {
+        try {
+            mRtcEngine = RtcEngine.create(baseContext, APP_ID, mRtcEventHandler)
+        } catch (e: Exception) {
+        }
+        var errCode = mRtcEngine!!.joinChannel(TOKEN, CHANNEL, "", 0)
 
     }
 
