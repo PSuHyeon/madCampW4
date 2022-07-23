2777d18b101fddd8c52e246a9e487336a7dd619b
editadiary <blessedita@kaist.ac.kr>
2022-07-23 17:39:09 +0900

    merged-stt

--- app/src/main/java/com/example/novelchat/CallTest.kt
+++ app/src/main/java/com/example/novelchat/CallTest.kt
@@ -8,10 +8,15 @@ import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.Manifest
+import android.content.Intent
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.util.Log
+import android.widget.TextView
 import java.lang.Exception
 
-class CallTest : AppCompatActivity() {
+class CallTest : AppCompatActivity(), RecognitionListener {
     private val PERMISSION_REQ_ID_RECORD_AUDIO = 22
 //    private val PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1
 
@@ -24,6 +29,7 @@ class CallTest : AppCompatActivity() {
     private var mRtcEngine: RtcEngine ?= null
     private val mRtcEventHandler = object : IRtcEngineEventHandler() {
     }
+    private var speech: SpeechRecognizer? = null
 
     private fun checkSelfPermission(permission: String, requestCode: Int): Boolean {
         if (ContextCompat.checkSelfPermission(this, permission) !=
@@ -37,6 +43,8 @@ class CallTest : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), 1);
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_test)
 
@@ -58,6 +66,91 @@ class CallTest : AppCompatActivity() {
         }
         var errCode = mRtcEngine!!.joinChannel(TOKEN, CHANNEL, "", 0)
 
+    }
+
+    fun clear(view: android.view.View) {
+        findViewById<TextView>(R.id.result).text = "";
+        findViewById<TextView>(R.id.log).text = "";
+    }
+
+    fun startRecognition(view: android.view.View) {
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech?.setRecognitionListener(this);
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+
+        speech?.startListening(intent);
+    }
+
+    fun stopRecognition(view: android.view.View) {
+        val view = findViewById<TextView>(R.id.log)
+        view.append("Stop button pressed\n")
+        speech?.stopListening();
+        view.append(".stopListening() called\n")
+    }
+
+    override fun onReadyForSpeech(params: Bundle?) {
+        val view = findViewById<TextView>(R.id.log)
+        view.append("onReadyForSpeech event\n")
+    }
+
+    override fun onRmsChanged(rmsdB: Float) {
+    }
+
+    override fun onBufferReceived(buffer: ByteArray?) {
+    }
+
+    override fun onPartialResults(partialResults: Bundle?) {
+        val matches = partialResults?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+
+        val view = findViewById<TextView>(R.id.result)
+        view.text = matches?.get(0)
+    }
+
+    override fun onResults(results: Bundle?) {
+        val matches = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+
+        val view = findViewById<TextView>(R.id.result)
+        view.text = matches?.get(0)
+    }
+
+    override fun onEvent(eventType: Int, params: Bundle?) {
+    }
+
+    override fun onBeginningOfSpeech() {
+        val view = findViewById<TextView>(R.id.log)
+        view.append("onBeginningOfSpeech event\n")
+    }
+
+    override fun onEndOfSpeech() {
+        val view = findViewById<TextView>(R.id.log)
+        view.append("onEndOfSpeech event\n")
+    }
+
+    override fun onError(error: Int) {
+        val message = getErrorText(error)
+        Log.d("SPEECH", message)
+
+        val view = findViewById<TextView>(R.id.log)
+        view.append("onError event: $message\n")
+    }
+
+    private fun getErrorText(errorCode: Int): String {
+        return when (errorCode) {
+            SpeechRecognizer.ERROR_AUDIO -> "ERROR_AUDIO"
+            SpeechRecognizer.ERROR_CLIENT -> "ERROR_CLIENT"
+            SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS -> "ERROR_INSUFFICIENT_PERMISSIONS"
+            SpeechRecognizer.ERROR_NETWORK -> "ERROR_NETWORK"
+            SpeechRecognizer.ERROR_NETWORK_TIMEOUT -> "ERROR_NETWORK_TIMEOUT"
+            SpeechRecognizer.ERROR_NO_MATCH -> "ERROR_NO_MATCH"
+            SpeechRecognizer.ERROR_RECOGNIZER_BUSY -> "ERROR_RECOGNIZER_BUSY"
+            SpeechRecognizer.ERROR_SERVER -> "ERROR_SERVER"
+            SpeechRecognizer.ERROR_SPEECH_TIMEOUT -> "ERROR_SPEECH_TIMEOUT"
+            else -> "Didn't understand, please try again."
+        }
     }
 
 }
