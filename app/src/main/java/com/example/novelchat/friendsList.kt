3c59223150e271417ffb0a7574ff495b39e225cc
suhyeonPark <0111suhyeon@kaist.ac.kr>
2022-07-28 19:37:29 +0900

    저장됨 표시되게 설정함

--- app/src/main/java/com/example/novelchat/friendsList.kt
+++ app/src/main/java/com/example/novelchat/friendsList.kt
@@ -184,8 +184,8 @@ class friendsList : AppCompatActivity() {
     }
 
     override fun onDetachedFromWindow() {
-        mSocket.emit("iamout", id)
         super.onDetachedFromWindow()
+        mSocket.emit("iamout", id)
     }
 }
 
